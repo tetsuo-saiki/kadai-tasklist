0e550fdc03dceacb43be183b985e5a9b4ac40a59
tetsuo.saiki <paso-ichio.mlkre@hotmail.co.jp>
2018-03-20 12:14:07 +0000

    change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-        <title>MessageBoard</title>
+        <title>TaskList</title>
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
